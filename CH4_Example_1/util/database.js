026adb83647f946db8c322e1cad07c5bc2913183
AbdullahSalahA <eng.a.sala77@gmail.com>
2020-05-17 15:10:44 +0200

    CH12_Done_mongoDb

--- CH4_Example_1/util/database.js
+++ CH4_Example_1/util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let _db;
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://AbdullahSalah:4Jk2nQPavdh3F04W@cluster0-xjafz.mongodb.net/shop?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected!');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+};
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No data base found !';
+}
 
-const sequelize = new Sequelize('node_complete', 'root', 'mySqL87K$F0D', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
